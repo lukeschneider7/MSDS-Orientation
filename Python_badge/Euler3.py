f16421026c30bc6728dedc2387c467ca012f0ca0
John Doe <lukeschneider7@yahoo.com>
2024-06-14 16:23:52 -0400

    Euler 3

--- Python_badge/Euler3.py
+++ Python_badge/Euler3.py
@@ -1,4 +1,13 @@
-x = 2
-print(x)
+### Find The Largest Prime Factor of 600851475143
 
+def largest_prime_factor(value):
+    factor = 2
+    while factor * factor <= value:
+        if value % factor!=0:
+            factor += 1
+        else:
+            value //= factor
+    return value
+
+print(largest_prime_factor(600851475143))
     
