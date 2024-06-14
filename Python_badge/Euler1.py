0ac512339f6bec6844b5ad8bfcd1b048c27829f3
John Doe <lukeschneider7@yahoo.com>
2024-06-14 15:20:20 -0400

    Project Euler

---
+++ Python_badge/Euler1.py
@@ -0,0 +1,11 @@
+count=0
+summ = 0
+
+
+while count<1000:
+    if count%3 == 0 or count%5==0:
+        summ+= count
+    count += 1
+
+
+print(summ)
---
+++ Python_badge/Euler2.py
@@ -0,0 +1,8 @@
+fib = 1
+summ = 0
+
+while fib < 400:
+    summ += fib
+fib += fib
+
+print(summ)
