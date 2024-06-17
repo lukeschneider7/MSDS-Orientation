913382a4bb3d480f740b37bbdbbc05ec95ebda14
John Doe <lukeschneider7@yahoo.com>
2024-06-16 21:50:03 -0400

    Random Distributions

--- Python_badge/ran_distributions.py
+++ Python_badge/ran_distributions.py
@@ -6,13 +6,26 @@ print("yes")
 
 #Binomial distribution
 random_bin_nums = np.random.binomial(n=200, p=.5, size=200) 
-plot.hist(random_bin_nums, bins=40, density=True, color='r')
+plot.hist(random_bin_nums, bins=40, color='r')
+plot.xlabel('Value')
+plot.ylabel('Frequency')
+plot.title("Binomial Distribution n=200, p=.5")
 plot.show()
 
 #Poisson distribution
-random_poisson_nums = np.random.poisson(5, 100)
+random_poisson_nums = np.random.poisson(lam=5, size=200)
+plot.hist(random_poisson_nums, bins=40, color='g')
+plot.title("Possion Distribution lambda=5")
+plot.xlabel('Value')
+plot.ylabel('Frequency')
+plot.show()
 
 #Normal distribution
 random_norm_nums = np.random.normal(loc=0, scale=1, size=200)
-plot.hist(random_norm_nums, bins=40, density=True, color='b')
+plot.hist(random_norm_nums, bins=40, color='b')
+plot.xlabel('Value')
+plot.ylabel('Frequency')
+plot.title("Normal Distribution mean=0, std.dev=1")
 plot.show()
+
+
